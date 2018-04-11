45129a299e9d82186a204efea1a5dc503b746fa8
Joe Herbert <djneonc@Gmail.com>
2018-04-10 20:52:38 -0600

    Initial commit

---
+++ FrameTimerZero/FrameTimerZero.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		FrameTimerZero.cpp
+ Created:	4/10/2018 8:32:51 PM
+ Author:	djneo
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "FrameTimerZero.h"
+
+
---
+++ FrameTimerZero/FrameTimerZero.h
@@ -0,0 +1,19 @@
+/*
+ Name:		FrameTimerZero.h
+ Created:	4/10/2018 8:32:51 PM
+ Author:	djneo
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _FrameTimerZero_h
+#define _FrameTimerZero_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
